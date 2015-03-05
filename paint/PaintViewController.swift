78bc7507460df8570dae76c495298ee3915ab74e
Andrew Brandt <abrandt@makeschool.com>
2015-03-05 13:20:56 -0800

    toolbar buttons, clear canvas implemented

--- paint/PaintViewController.swift
+++ paint/PaintViewController.swift
@@ -11,19 +11,64 @@ import UIKit
 class PaintViewController: UIViewController {
     
     @IBOutlet weak var canvas: UIView!
+    var currentColor: UIColor = UIColor.redColor()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //setup toolbar buttons
+        var clearButton = UIBarButtonItem(title:"Clear", style: .Bordered, target: self, action: Selector("clearCanvas"))
+        clearButton.width = 80
+        
+        var selectColorButton = UIBarButtonItem(title:"Choose Color", style: .Bordered, target: self, action: Selector("setColor"))
+        selectColorButton.width = 100
+        
+        var sizeButton = UIBarButtonItem(title:"Brush Size", style: .Bordered, target: self, action: Selector("setSize"))
+        sizeButton.width = 100
+        
+        var toolbarItems = [clearButton, selectColorButton, sizeButton]
+        
+        self.toolbarItems = toolbarItems
+        self.navigationController?.toolbarHidden = false
     }
 
     @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
+        //get location of touch
         var touch = recognizer.locationInView(canvas)
-        println("x: \(touch.x) y: \(touch.y)")
+        //println("x: \(touch.x) y: \(touch.y)")
+        
+        //make CGRect for CanvasView frame
         var panRect = CGRectMake(touch.x, touch.y, 10, 10)
         var paint = CanvasView(frame:panRect, color:UIColor.redColor())
         canvas.addSubview(paint)
         
+    }
+    
+    @IBAction func handleTap(recognizer: UITapGestureRecognizer) {
+        //get location of touch
+        var touch = recognizer.locationInView(canvas)
+        //println("x: \(touch.x) y: \(touch.y)")
+        
+        //make CGRect for CanvasView frame
+        var panRect = CGRectMake(touch.x, touch.y, 10, 10)
+        var paint = CanvasView(frame:panRect, color:UIColor.redColor())
+        canvas.addSubview(paint)
+        
+    }
+    
+    func clearCanvas() {
+        for obj in canvas.subviews {
+            if let paint = obj as? CanvasView {
+                paint.removeFromSuperview()
+            }
+        }
+    }
+    
+    func setColor() {
+        
+    }
+    
+    func setSize() {
+    
     }
 }
