45bb35ee2861bc727bdaf7d2809fe6de7bc58e11
Andrew Brandt <abrandt@makeschool.com>
2015-03-05 16:40:40 -0800

    picker for brush size implemented

--- paint/CanvasView.swift
+++ paint/CanvasView.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class CanvasView: UIView {
     var color: UIColor!
+    var newPath: Bool
     
-    init(frame: CGRect, color: UIColor) {
+    init(frame: CGRect, color: UIColor, newPath: Bool) {
+        self.newPath = newPath
         super.init(frame: frame)
-        self.backgroundColor = UIColor.clearColor()
+        self.backgroundColor = color
         self.color = color
     }
 
@@ -23,8 +25,26 @@ class CanvasView: UIView {
     
     override func drawRect(rect:CGRect) {
         var context = UIGraphicsGetCurrentContext()
-        CGContextAddEllipseInRect(context, rect)
+        
+        // This with draw unjoined circles
+        //
+        // CGContextAddEllipseInRect(context, rect)
+        // CGContextSetFillColor(context, CGColorGetComponents(self.color.CGColor))
+        // CGContextFillPath(context)
+        //
+        
+        if (self.newPath) {
+            CGContextBeginPath(context)
+            CGContextMoveToPoint(context, rect.origin.x, rect.origin.y)
+        }
+        
+        CGContextSetStrokeColorWithColor(context, self.color.CGColor)
         CGContextSetFillColor(context, CGColorGetComponents(self.color.CGColor))
-        CGContextFillPath(context)
+        //CGContextAddRect(context, rect)
+        //CGContextAddPath(context, CGPathCreateWithRect(rect, nil))
+        CGContextSetLineWidth(context, rect.width)
+        CGContextAddLineToPoint(context, rect.origin.x, rect.origin.y)
+        //CGContextDrawPath(context, kCGPathFillStroke)
+        //CGContextFillPath(context)
     }
 }
--- paint/PaintViewController.swift
+++ paint/PaintViewController.swift
@@ -8,28 +8,46 @@
 
 import UIKit
 
-class PaintViewController: UIViewController, FCColorPickerViewControllerDelegate {
+class PaintViewController: UIViewController, FCColorPickerViewControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var canvas: UIView!
     var currentColor: UIColor = UIColor.redColor()
+    var currentSize: CGFloat = 5.0
+    let brushSizes = ["Small", "Medium", "Large"]
+    var picker: UIPickerView!
+    var strokes: [CGPoint] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //setup toolbar buttons
         var clearButton = UIBarButtonItem(title:"Clear", style: .Bordered, target: self, action: Selector("clearCanvas"))
-        clearButton.width = 80
+        clearButton.width = 70
         
         var selectColorButton = UIBarButtonItem(title:"Choose Color", style: .Bordered, target: self, action: Selector("setColor"))
-        selectColorButton.width = 120
+        selectColorButton.width = 100
         
         var sizeButton = UIBarButtonItem(title:"Brush Size", style: .Bordered, target: self, action: Selector("setSize"))
-        sizeButton.width = 120
+        sizeButton.width = 100
         
         var toolbarItems = [clearButton, selectColorButton, sizeButton]
         
         self.toolbarItems = toolbarItems
         self.navigationController?.toolbarHidden = false
+        
+        //init brush size picker
+        var width = UIScreen.mainScreen().bounds.width
+        var height = UIScreen.mainScreen().bounds.height
+        var pickerHeight = height - 250
+        var pickerWidth = width * 3/4
+        var x = width/2 - pickerWidth/2
+        
+        self.picker = UIPickerView(frame: CGRectMake(x, pickerHeight, pickerWidth, 200))
+        self.picker.delegate = self
+        self.picker.showsSelectionIndicator = true
+        self.picker.backgroundColor = UIColor.lightGrayColor()
+        self.picker.hidden = true
+        canvas.addSubview(self.picker)
     }
 
     @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
@@ -37,10 +55,11 @@ class PaintViewController: UIViewController, FCColorPickerViewControllerDelegate
         var touch = recognizer.locationInView(canvas)
         //println("x: \(touch.x) y: \(touch.y)")
         
-        //make CGRect for CanvasView frame
-        var panRect = CGRectMake(touch.x, touch.y, 10, 10)
-        var paint = CanvasView(frame:panRect, color:self.currentColor)
-        canvas.addSubview(paint)
+        if recognizer.state == .Began {
+            self.paint(touch, end:true)
+        } else {
+            self.paint(touch, end:false)
+        }
         
     }
     
@@ -48,12 +67,15 @@ class PaintViewController: UIViewController, FCColorPickerViewControllerDelegate
         //get location of touch
         var touch = recognizer.locationInView(canvas)
         //println("x: \(touch.x) y: \(touch.y)")
-        
+        self.paint(touch, end: true)
+    }
+    
+    func paint(loc: CGPoint, end: Bool) {
         //make CGRect for CanvasView frame
-        var panRect = CGRectMake(touch.x, touch.y, 10, 10)
-        var paint = CanvasView(frame:panRect, color:self.currentColor)
+        var panRect = CGRectMake(loc.x - self.currentSize, loc.y - self.currentSize, 2*self.currentSize, 2*self.currentSize)
+        var paint = CanvasView(frame:panRect, color:self.currentColor, newPath: end)
+        //add to view
         canvas.addSubview(paint)
-        
     }
     
     func clearCanvas() {
@@ -73,8 +95,11 @@ class PaintViewController: UIViewController, FCColorPickerViewControllerDelegate
     }
     
     func setSize() {
-    
+        canvas.bringSubviewToFront(self.picker)
+        self.picker.hidden = false
     }
+    
+    //pragma MARK: - FCColorPickerViewControllerDelegate methods
     
     func colorPickerViewController(colorPicker: FCColorPickerViewController!, didSelectColor color: UIColor!) {
         self.currentColor = color
@@ -85,4 +110,37 @@ class PaintViewController: UIViewController, FCColorPickerViewControllerDelegate
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    //pragma MARK: - UIPickerViewDataSource methods
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.brushSizes.count
+    }
+    
+    //pragma MARK: - UIPickerViewDelegate methods
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return self.brushSizes[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch (row) {
+            case 0:
+                self.currentSize = 5.0
+                break;
+            case 1:
+                self.currentSize = 12.0
+                break;
+            case 2:
+                self.currentSize = 20.0
+                break;
+            default:
+                break;
+        }
+        //this breaks, no stack trace?
+        //pickerView.removeFromSuperview()
+    }
 }
